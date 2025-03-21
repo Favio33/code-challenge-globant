ec8d8740530c5146ac332795e18f70f7911eefbb
favio33 <favio.varillas36@gmail.com>
2025-03-21 16:03:38 -0500

    update ddl

--- init/init_db.sql
+++ init/init_db.sql
@@ -1,24 +1,28 @@
+drop table if exists public.hired_employees;
+drop table if exists public.departments;
+drop table if exists public.jobs;
+
 create table public.departments (
-	id bigint not null,
+	id serial,
 	department varchar(50) not null,
-	last_updated timestamp without time zone,
+	last_updated timestamp without time zone default current_timestamp,
 	primary key (id)
 );
 
 create table public.jobs (
-	id bigint not null,
+	id serial,
 	job varchar(50) not null,
-	last_updated timestamp without time zone,
+	last_updated timestamp without time zone default current_timestamp,
 	primary key (id)
 );
 
 create table public.hired_employees (
-	id bigint not null,
+	id serial,
 	name varchar(100) not null,
 	datetime timestamp without time zone,
-	department_id bigint,
-	job_id bigint,
-	last_updated timestamp without time zone,
+	department_id serial,
+	job_id serial,
+	last_updated timestamp without time zone default current_timestamp,
 	primary key (id),
 	constraint fk_job foreign key (job_id) references jobs(id),
 	constraint fk_department foreign key (department_id) references departments(id)
