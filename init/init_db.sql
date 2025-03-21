8860a6b1b4c9019fffa867811dd2f2cc3bb5a719
favio33 <favio.varillas36@gmail.com>
2025-03-20 23:02:04 -0500

    update text cols to varchar

--- init/init_db.sql
+++ init/init_db.sql
@@ -1,20 +1,20 @@
 create table public.departments (
 	id bigint not null,
-	department text not null,
+	department varchar(50) not null,
 	last_updated timestamp without time zone,
 	primary key (id)
 );
 
 create table public.jobs (
 	id bigint not null,
-	job text not null,
+	job varchar(50) not null,
 	last_updated timestamp without time zone,
 	primary key (id)
 );
 
 create table public.hired_employees (
 	id bigint not null,
-	name text not null,
+	name varchar(100) not null,
 	datetime timestamp without time zone,
 	department_id bigint,
 	job_id bigint,
