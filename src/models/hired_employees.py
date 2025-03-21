33846e002219cab376a68ed3e1d08861c03dee38
favio33 <favio.varillas36@gmail.com>
2025-03-21 16:04:38 -0500

    first version models

---
+++ src/models/hired_employees.py
@@ -0,0 +1,35 @@
+from sqlalchemy import BigInteger, Column, DateTime, ForeignKey, String, func
+
+from database.db import Base
+
+
+class HiredEmployees(Base):
+
+    __tablename__ = 'hired_employees'
+
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String, nullable=False)
+    datetime = Column(DateTime, nullable=True)
+    department_id = Column(BigInteger, ForeignKey("Department.id"))
+    job_id = Column(BigInteger, ForeignKey("Job.id"))
+    last_updated = Column(DateTime, nullable=False, server_default=func.now())
+
+    def __init__(self, name: str, datetime: str, department_id: int, job_id: int, id: int = None):
+        self.name = name
+        self.datetime = datetime
+        self.department_id = department_id
+        self.job_id = job_id
+        self.id = id
+
+    def __repr__(self):
+        return f"""Department(id={self.id}, name={self.name}, datetime={self.datetime},
+            department_id={self.department_id}, job_id={self.job_id}, last_updated={self.last_updated})"""  # noqa: E501
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'datetime': self.datetime,
+            'department_id': self.department_id,
+            'job_id': self.job_id
+        }
