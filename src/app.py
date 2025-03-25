a54dbed5c95d1bb2ce5f42d299c1bec313b50dbb
favio33 <favio.varillas36@gmail.com>
2025-03-25 11:05:20 -0500

    add static web for loading csv

--- src/app.py
+++ src/app.py
@@ -1,18 +1,25 @@
-from flask import Flask
+import os
+
+from flask import Flask, send_from_directory
 
 from config import configuration
 from routes import aggregations, department, hired_employees, jobs
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(configuration['development'])
+    app = Flask(__name__, static_folder='static')
+    env = os.getenv("FLASK_ENV", "development")
+    app.config.from_object(configuration[env])
 
     # Blueprints
     app.register_blueprint(department.department_bp, url_prefix='/api/departments')
     app.register_blueprint(jobs.jobs_bp, url_prefix='/api/jobs')
     app.register_blueprint(hired_employees.employees_bp, url_prefix='/api/employees')
     app.register_blueprint(aggregations.aggregations_bp, url_prefix='/api/aggregations')
+
+    @app.route('/')
+    def serve_index():
+        return send_from_directory(app.static_folder, 'index.html')
 
     # Error handlers
     app.register_error_handler(404, page_not_found)
